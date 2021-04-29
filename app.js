8cef3310c6129647c3fce5c106ac554bc91abc04
Manikantha-M <manikanta.iiitnuz@gmail.com>
2021-04-29 14:43:53 +0530

    navbar improved

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
 
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 630) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
